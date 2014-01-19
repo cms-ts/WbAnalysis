4c56b02e8b3f93d9c5d36e911b8fe7b8f746fc19
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2014-01-19 19:13:02 +0100

    add v01 info file

---
+++ WbSkim/macros/LumiInfo_v01.h
@@ -0,0 +1,82 @@
+string version = "v01";
+
+//////////////////// Integrated luminosity in 1/picobarn
+
+double Lumi2012_ele  = 19739.0; // full 2012 electrons luminosity
+double Lumi2012_muon = 19770.0; // full 2012 muons luminosity
+
+// single period lumi, same for electrons and muons dataset in 1/picobarn
+
+double Lumi_2012A_22Jan =  889.362;
+double Lumi_2012B_22Jan = 4418.000;
+double Lumi_2012C_22Jan = 7114.000;
+double Lumi_2012D_22Jan = 7318.000;
+
+//////////////////////// DY MadGraph
+
+double Ngen_dy = 30459503;
+double Xsec_dy = 3503.71; // NNLO
+double eXsec_dy = 0;
+
+//////////////////////// TTbar
+
+double Ngen_tt = 6923750;
+double Xsec_tt = 225.197; // NLO
+double eXsec_tt = 0.07*Xsec_tt;
+
+//////////////////////// ZZ
+
+double Ngen_zz = 9799908;
+double Xsec_zz = 8.059; // NLO with CTEQ
+double eXsec_zz = 0.15*Xsec_zz; 
+
+//////////////////////// WZ
+
+double Ngen_wz = 10000283;
+double Xsec_wz = 33.21;    // NLO with CTEQ
+double eXsec_wz = 0.15*Xsec_wz; 
+
+//////////////////////// QCD
+
+double Ngen_qcd = 7529312;
+double Xsec_qcd = 3.64E8; // search the NLO !
+double eXsec_qcd = 0.15*Xsec_qcd; 
+
+//////////////////////// WW
+
+double Ngen_ww = 10000431;
+double Xsec_ww = 54.838; // NLO with CTEQ
+double eXsec_ww = 0.15*Xsec_ww; 
+
+//////////////////////// Wj
+
+double Ngen_wj = 57709905;
+double Xsec_wj = 31200; // NLO with CTEQ
+double eXsec_wj = 0.15*Xsec_wj; 
+
+//////////////////////// W1j
+
+double Ngen_w1j = 23141598;
+double Xsec_w1j = 31200; // NLO with CTEQ
+double eXsec_w1j = 0.15*Xsec_w1j; 
+
+//////////////////////// W2j
+
+double Ngen_w2j = 34044921;
+double Xsec_w2j = 31200; // NLO with CTEQ
+double eXsec_w2j = 0.15*Xsec_w2j; 
+
+//////////////////////// W3j
+
+double Ngen_w3j = 15539503;
+double Xsec_w3j = 31200; // NLO with CTEQ
+double eXsec_w3j = 0.15*Xsec_w3j; 
+
+//////////////////////// W4j
+
+double Ngen_w4j = 13382803;
+double Xsec_w4j = 31200; // NLO with CTEQ
+double eXsec_w4j = 0.15*Xsec_w4j; 
+
+///////////////////////
+
