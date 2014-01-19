24c352ffefa72eac09b7397672a28e8f0e8fb9b2
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2014-01-19 20:15:59 +0100

    one more DoubleMu file in v01

--- WbSkim/macros/LumiInfo_v01.h
+++ WbSkim/macros/LumiInfo_v01.h
@@ -3,7 +3,7 @@ string version = "v01";
 //////////////////// Integrated luminosity in 1/picobarn
 
 double Lumi2012_ele  = 19739.0; // full 2012 electrons luminosity
-double Lumi2012_muon = 19770.0; // full 2012 muons luminosity
+double Lumi2012_muon = 19774.0; // full 2012 muons luminosity
 
 // single period lumi, same for electrons and muons dataset in 1/picobarn
 
