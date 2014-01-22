79e054376d0f83a1f4812c0295739a2933af07a2
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2014-01-22 15:44:37 +0100

    add macros for reweighting of W+j

---
+++ WbSkim/macros/hmerge.C
@@ -0,0 +1,194 @@
+
+#include <string.h>
+#include "TChain.h"
+#include "TFile.h"
+#include "TH1.h"
+#include "TTree.h"
+#include "TKey.h"
+#include "Riostream.h"
+
+#include "LumiInfo_v01.h"
+
+TList *FileList;
+TFile *Target;
+
+void hmerge_(TDirectory *target, TList *sourcelist, double crossArray[]);
+
+//string path = "/gpfs/cms/users/schizzi/Wbb2012/test/data/";
+string path = "/gpfs/cms/users/schizzi/Wbb2012/test/GDR/data/";
+
+void hmerge(string title="W") {
+
+  FileList = new TList();
+  double crossSections[10];
+
+  if (title=="W") {
+    Target = TFile::Open((path + "/" + version + "/" + "Wj_merge.root").c_str(), "RECREATE");
+
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "Wj.root").c_str()));
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "W1j.root").c_str()));
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "W2j.root").c_str()));
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "W3j.root").c_str()));
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "W4j.root").c_str()));
+
+    crossSections[0] = (Xsec_wj / Ngen_wj);
+    crossSections[1] = (Xsec_w1j / Ngen_w1j) * (Xsec_wj / 30400.);
+    crossSections[2] = (Xsec_w2j / Ngen_w2j) * (Xsec_wj / 30400.);
+    crossSections[3] = (Xsec_w3j / Ngen_w3j) * (Xsec_wj / 30400.);
+    crossSections[4] = (Xsec_w4j / Ngen_w4j) * (Xsec_wj / 30400.);
+
+    crossSections[0] = crossSections[0] / (Xsec_wj / Ngen_wj);
+    crossSections[1] = crossSections[1] / (Xsec_wj / Ngen_wj);
+    crossSections[2] = crossSections[2] / (Xsec_wj / Ngen_wj);
+    crossSections[3] = crossSections[3] / (Xsec_wj / Ngen_wj);
+    crossSections[4] = crossSections[4] / (Xsec_wj / Ngen_wj);
+  }
+
+  if (title=="T") {
+    Target = TFile::Open((path + "/" + version + "/" + "T_merge.root").c_str(), "RECREATE");
+
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "TBarToLeptons_t.root").c_str()));
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "TBarToLeptons_s.root").c_str()));
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "TToLeptons_t.root").c_str()));
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "TToLeptons_s.root").c_str()));
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "TBarToThadWlep_tW.root").c_str()));
+    FileList->Add(TFile::Open((path + "/" + version + "/" + "TToThadWlep_tW.root").c_str()));
+
+    crossSections[0] = (Xsec_tbar_t / Ngen_tbar_t);
+    crossSections[1] = (Xsec_tbar_s / Ngen_tbar_s) / crossSections[0];
+    crossSections[2] = (Xsec_t_t / Ngen_t_t) / crossSections[0];
+    crossSections[3] = (Xsec_t_s / Ngen_t_s) / crossSections[0];
+    crossSections[4] = (Xsec_tbar_tw / Ngen_tbar_tw) / crossSections[0];
+    crossSections[5] = (Xsec_t_tw / Ngen_t_tw) / crossSections[0];
+
+    crossSections[0] = crossSections[0] / (Xsec_tbar_t / Ngen_tbar_t);
+    crossSections[1] = crossSections[1] / (Xsec_tbar_t / Ngen_tbar_t);
+    crossSections[2] = crossSections[2] / (Xsec_tbar_t / Ngen_tbar_t);
+    crossSections[3] = crossSections[3] / (Xsec_tbar_t / Ngen_tbar_t);
+    crossSections[4] = crossSections[4] / (Xsec_tbar_t / Ngen_tbar_t);
+    crossSections[5] = crossSections[5] / (Xsec_tbar_t / Ngen_tbar_t);
+  }
+
+  cout << "going to call merging routine..." << endl;
+  hmerge_( Target, FileList, crossSections );
+  cout << "done." << endl;
+
+}
+
+void hmerge_( TDirectory *target, TList *sourcelist, double crossArray[] ) {
+
+  cout << "Target path: " << target->GetPath() << endl;
+  TString rpath( (char*)strstr( target->GetPath(), ":" ) );
+  rpath.Remove( 0, 2 );
+
+  TFile *first_source = (TFile*)sourcelist->First();
+
+  first_source->cd( rpath );
+  TDirectory *current_sourcedir = gDirectory;
+  //gain time, do not add the objects in the list in memory
+  Bool_t status = TH1::AddDirectoryStatus();
+  TH1::AddDirectory(kFALSE);
+
+  // loop over all keys in this directory
+  TChain *globChain = 0;
+  TIter nextkey( current_sourcedir->GetListOfKeys() );
+  TKey *key, *oldkey=0;
+  while ( (key = (TKey*)nextkey())) {
+
+    //keep only the highest cycle number for each key
+    if (oldkey && !strcmp(oldkey->GetName(),key->GetName())) continue;
+
+    // read object from first source file
+    first_source->cd( rpath );
+    TObject *obj = key->ReadObj();
+
+    if ( obj->IsA()->InheritsFrom( "TH1" ) ) {
+      // descendant of TH1 -> merge it
+
+      cout << "Merging histogram " << obj->GetName() << endl;
+      TH1 *h1 = (TH1*)obj;
+      h1->Sumw2();
+
+      // Scale by the cross-section factor
+      h1->Scale(crossArray[0]);
+
+      // loop over all source files and add the content of the
+      // correspondant histogram to the one pointed to by "h1"
+      TFile *nextsource = (TFile*)sourcelist->After( first_source );
+
+      int q = 1; // This keeps track of which
+                 // cross section factor to use
+      while ( nextsource ) {
+        // make sure we are at the correct directory level by cd'ing to rpath
+        nextsource->cd( rpath );
+        TKey *key2 = (TKey*)gDirectory->GetListOfKeys()->FindObject(h1->GetName());
+        if (key2) {
+           TH1 *h2 = (TH1*)key2->ReadObj();
+           h2->Sumw2();
+
+           // Scale by the cross-section factor before adding.
+           h2->Scale(crossArray[q]);
+           h1->Add( h2 );
+           q++;
+           delete h2;
+        }
+
+        nextsource = (TFile*)sourcelist->After( nextsource );
+      }
+
+    } else if ( obj->IsA()->InheritsFrom( "TTree" ) ) {
+
+      // loop over all source files create a chain of Trees "globChain"
+      const char* obj_name= obj->GetName();
+
+      globChain = new TChain(obj_name);
+      globChain->Add(first_source->GetName());
+      TFile *nextsource = (TFile*)sourcelist->After( first_source );
+      while ( nextsource ) {
+        globChain->Add(nextsource->GetName());
+        nextsource = (TFile*)sourcelist->After( nextsource );
+      }
+
+    } else if ( obj->IsA()->InheritsFrom( "TDirectory" ) ) {
+
+      cout << "Found subdirectory " << obj->GetName() << endl;
+
+      // create a new subdir of same name and title in the target file
+      target->cd();
+      TDirectory *newdir = target->mkdir( obj->GetName(), obj->GetTitle() );
+
+      // newdir is now the starting point of another round of merging
+      // newdir still knows its depth within the target file via
+      // GetPath(), so we can still figure out where we are in the recursion
+      hmerge_( newdir, sourcelist,crossArray );
+
+    } else {
+
+      // object is of no type that we know or can handle
+      cout << "Unknown object type, name: "
+           << obj->GetName() << " title: " << obj->GetTitle() << endl;
+
+    }
+
+    // now write the merged histogram (which is "in" obj) to the target file
+    // note that this will just store obj in the current directory level,
+    // which is not persistent until the complete directory itself is stored
+    // by "target->Write()" below
+    if ( obj ) {
+      target->cd();
+
+      //!!if the object is a tree, it is stored in globChain...
+        if(obj->IsA()->InheritsFrom( "TTree" ))
+          globChain->Merge(target->GetFile(),0,"keep");
+        else
+        obj->Write( key->GetName() );
+    }
+
+  } // while ( ( TKey *key = (TKey*)nextkey() ) )
+
+  // save modifications to target file
+  target->SaveSelf(kTRUE);
+  TH1::AddDirectory(status);
+
+}
+
---
+++ WbSkim/macros/hmerge_test.C
@@ -0,0 +1,77 @@
+
+#include "LumiInfo_v01.h"
+
+void hmerge_test() {
+
+TFile f0("data/v01/Wj.root");
+TFile f1("data/v01/W1j.root");
+TFile f2("data/v01/W2j.root");
+TFile f3("data/v01/W3j.root");
+TFile f4("data/v01/W4j.root");
+TFile f5("data/v01/Wj_merge.root");
+
+f0.cd("demoEle");
+TH1F* h0x = (TH1F*)gDirectory->Get("h_nmult0");
+TH1F* h0 = (TH1F*)gDirectory->Get("h_nmult1");
+f1.cd("demoEle");
+TH1F* h1 = (TH1F*)gDirectory->Get("h_nmult1");
+f2.cd("demoEle");
+TH1F* h2 = (TH1F*)gDirectory->Get("h_nmult1");
+f3.cd("demoEle");
+TH1F* h3 = (TH1F*)gDirectory->Get("h_nmult1");
+f4.cd("demoEle");
+TH1F* h4 = (TH1F*)gDirectory->Get("h_nmult1");
+f5.cd("demoEle");
+TH1F* h5 = (TH1F*)gDirectory->Get("h_nmult1");
+
+h0x->Sumw2();
+h0->Sumw2();
+h1->Sumw2();
+h2->Sumw2();
+h3->Sumw2();
+h4->Sumw2();
+h5->Sumw2();
+
+h0x->Scale(Xsec_wj/Ngen_wj);
+h0->Scale(Xsec_wj/Ngen_wj);
+
+h1->Scale((Xsec_w1j/Ngen_w1j)*(Xsec_wj/30400.));
+h2->Scale((Xsec_w2j/Ngen_w2j)*(Xsec_wj/30400.));
+h3->Scale((Xsec_w3j/Ngen_w3j)*(Xsec_wj/30400.));
+h4->Scale((Xsec_w4j/Ngen_w4j)*(Xsec_wj/30400.));
+
+h5->Scale(Xsec_wj/Ngen_wj);
+
+TCanvas* c = new TCanvas( "C", "MyCanvas C", 100, 0, 500, 500 );
+c->cd();
+
+h0x->SetFillColor(kCyan);
+h0x->SetMarkerStyle(24);
+h0x->GetXaxis()->SetRangeUser(0, 4);
+h0x->SetMinimum(0);
+h0x->SetMaximum(28);
+
+h0x->DrawCopy("E2");
+
+h1->DrawCopy("E1SAME");
+h2->DrawCopy("E1SAME");
+h3->DrawCopy("E1SAME");
+h4->DrawCopy("E1SAME");
+
+h5->SetLineColor(kRed);
+
+h5->DrawCopy("E1SAME");
+
+for (int i=1;i<=5;i++) {
+cout << i << " ";
+cout << h0x->GetBinContent(i) << "+-" << h0x->GetBinError(i) << " ";
+cout << h0->GetBinContent(i) << "+-" << h0->GetBinError(i) << " ";
+cout << h1->GetBinContent(i) << "+-" << h1->GetBinError(i) << " ";
+cout << h2->GetBinContent(i) << "+-" << h2->GetBinError(i) << " ";
+cout << h3->GetBinContent(i) << "+-" << h4->GetBinError(i) << " ";
+cout << h4->GetBinContent(i) << "+-" << h4->GetBinError(i) << " ";
+cout << h5->GetBinContent(i) << "+-" << h5->GetBinError(i) << " ";
+cout << endl;
+}
+
+}
