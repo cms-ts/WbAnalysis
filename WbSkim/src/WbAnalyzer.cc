719424d32f6a1ddeaa4342d4dd35d453a55110a5
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2014-02-20 22:23:42 +0100

    do not cut on trigger matching for QCD leptons

--- WbSkim/src/WbAnalyzer.cc
+++ WbSkim/src/WbAnalyzer.cc
@@ -1719,7 +1719,11 @@ void WbAnalyzer::produce (edm::Event & iEvent, const edm::EventSetup & iSetup) {
 
   for (pat::ElectronCollection::const_iterator ele = electrons->begin (); ele != electrons->end (); ++ele) {
     if (ele->pt()>30 && fabs(ele->eta())<2.1) {
-      if (ele->triggerObjectMatches().size()>0) vect_ele.push_back (*ele);
+      if (lepton_ == "electronQCD") {
+        vect_ele.push_back (*ele);
+      } else {
+        if (ele->triggerObjectMatches().size()>0) vect_ele.push_back (*ele);
+      }
       if (ele->triggerObjectMatches().size()>0) ntrgMatchesEle++;
     }
   }
@@ -1767,7 +1771,11 @@ void WbAnalyzer::produce (edm::Event & iEvent, const edm::EventSetup & iSetup) {
 
   for (pat::MuonCollection::const_iterator muon = muons->begin (); muon != muons->end (); ++muon) {
     if (muon->pt()>30 && fabs(muon->eta())<2.1) {
-      if (muon->triggerObjectMatches().size()>0) vect_muon.push_back (*muon);
+      if (lepton_ == "muonQCD") {
+        vect_muon.push_back (*muon);
+      } else {
+        if (muon->triggerObjectMatches().size()>0) vect_muon.push_back (*muon);
+      }
       if (muon->triggerObjectMatches().size()>0) ntrgMatchesMuo++;
     }
   }
