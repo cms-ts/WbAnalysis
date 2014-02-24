a28dee61b9663970d25e1531ee6e96ebfef1f076
Andrea Schizzi <andrea.schizzi@cern.ch>
2014-02-24 18:26:12 +0100

    scale factors macro

---
+++ WbSkim/test/efficiency/makeSF.C
@@ -0,0 +1,91 @@
+#include <iomanip>
+
+void makeSF() {
+
+  string pathFileData ="efficiency-data-WP80toHLT.root";
+  string pathFileMC   ="efficiency-mc-WP80toHLT.root";
+  
+  TFile *histodata = TFile::Open (pathFileData.c_str ());
+  histodata->cd ("WP80ToHLT/HLT_Ele27_WP80/fit_eff_plots");
+  TCanvas *dataEff;
+  gDirectory->GetObject ("probe_patEle_eta_probe_patEle_et_PLOT", dataEff);
+
+  TFile *histomc = TFile::Open (pathFileMC.c_str ());
+  histomc->cd ("WP80ToHLT/HLT_Ele27_WP80/fit_eff_plots");
+  TCanvas *mcEff;
+  gDirectory->GetObject ("probe_patEle_eta_probe_patEle_et_PLOT", mcEff);
+  
+  TH2D *hdata;
+  hdata = (TH2D*) dataEff->GetPrimitive("probe_patEle_eta_probe_patEle_et_PLOT");
+  hdata->Sumw2();
+
+  TH2D *hmc;
+  hmc = (TH2D*) mcEff->GetPrimitive("probe_patEle_eta_probe_patEle_et_PLOT");
+  hmc->Sumw2();
+
+  TH2D *hsf;
+  hsf = (TH2D *) hdata->Clone();
+  hsf->Sumw2();
+  hsf->Divide(hmc);
+
+  ofstream textfileData;
+  textfileData.open("ele_HLT_data.txt");
+
+  for (int i = 1; i<=hdata->GetNbinsX(); i++) {
+    for (int j = 1; j<=hdata->GetNbinsY(); j++) {
+      textfileData << std::fixed
+		 << std::setprecision(4)
+		 << std::setw(7) << hsf->GetXaxis()->GetBinLowEdge(i)
+		 << std::setw(8) << hsf->GetXaxis()->GetBinUpEdge (i)
+	         << std::setprecision(1)
+                 << std::setw(5) << hsf->GetYaxis()->GetBinLowEdge(j)
+		 << std::setw(7) << hsf->GetYaxis()->GetBinUpEdge (j)
+                 << std::setprecision(4)
+		 << std::setw(8) << hsf->GetBinContent(i,j)
+                 << std::setw(8) << hsf->GetBinError(i,j)
+		 << std::setw(8) << hsf->GetBinError(i,j) << endl;
+    }
+  }
+  textfileData.close();
+
+
+  ofstream textfileMC;
+  textfileMC.open("ele_HLT_mc.txt");
+
+  for (int i = 1; i<=hmc->GetNbinsX(); i++) {
+    for (int j = 1; j<=hmc->GetNbinsY(); j++) {
+      textfileMC << std::fixed
+		 << std::setprecision(4)
+		 << std::setw(7) << hsf->GetXaxis()->GetBinLowEdge(i)
+		 << std::setw(8) << hsf->GetXaxis()->GetBinUpEdge (i)
+	         << std::setprecision(1)
+                 << std::setw(5) << hsf->GetYaxis()->GetBinLowEdge(j)
+		 << std::setw(7) << hsf->GetYaxis()->GetBinUpEdge (j)
+                 << std::setprecision(4)
+		 << std::setw(8) << hsf->GetBinContent(i,j)
+                 << std::setw(8) << hsf->GetBinError(i,j)
+		 << std::setw(8) << hsf->GetBinError(i,j) << endl;
+    }
+  }
+  textfileMC.close();
+
+
+  ofstream textfileSF;
+  textfileSF.open("ele_HLT_sf.txt");
+  for (int i = 1; i<=hsf->GetNbinsX(); i++) {
+    for (int j = 1; j<=hsf->GetNbinsY(); j++) {
+      textfileSF << std::fixed
+		 << std::setprecision(4)
+		 << std::setw(7) << hsf->GetXaxis()->GetBinLowEdge(i)
+		 << std::setw(8) << hsf->GetXaxis()->GetBinUpEdge (i)
+	         << std::setprecision(1)
+                 << std::setw(5) << hsf->GetYaxis()->GetBinLowEdge(j)
+		 << std::setw(7) << hsf->GetYaxis()->GetBinUpEdge (j)
+                 << std::setprecision(4)
+		 << std::setw(8) << hsf->GetBinContent(i,j)
+                 << std::setw(8) << hsf->GetBinError(i,j)
+		 << std::setw(8) << hsf->GetBinError(i,j) << endl;
+    }
+  }
+  textfileSF.close();
+}
