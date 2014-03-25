8920e910255aefd99fbdb2e916e2dd0d3914547d
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2014-03-25 08:01:27 +0100

    add merge for GEN

---
+++ WbSkim/test/merge_gen_all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VERSION=v03
+
+if [ ! -z "$1" ]; then
+  VERSION=$1
+fi
+
+./merge.sh Wj_gen $VERSION
+./merge.sh W1j_gen $VERSION
+./merge.sh W2j_gen $VERSION
+./merge.sh W3j_gen $VERSION
+./merge.sh W4j_gen $VERSION
+
+./merge.sh Wj_gen-all $VERSION
+
+exit
