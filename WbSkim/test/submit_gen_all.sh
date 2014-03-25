596b80d7105b04d19ac458b56d73ba5fe2937545
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2014-03-25 15:51:51 +0100

    fix typo

--- WbSkim/test/submit_gen_all.sh
+++ WbSkim/test/submit_gen_all.sh
@@ -6,10 +6,10 @@ if [ ! -z "$1" ]; then
   VERSION=$1
 fi
 
-./submit_gen.sh Wj $VERSION
-./submit_gen.sh W1j $VERSION
-./submit_gen.sh W2j $VERSION
-./submit_gen.sh W3j $VERSION
-./submit_gen.sh W4j $VERSION
+./submit_gen.sh Wj_gen $VERSION
+./submit_gen.sh W1j_gen $VERSION
+./submit_gen.sh W2j_gen $VERSION
+./submit_gen.sh W3j_gen $VERSION
+./submit_gen.sh W4j_gen $VERSION
 
 exit
