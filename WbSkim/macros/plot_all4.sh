b46173ee6b44e42c583af3364cc29651b9a9af22
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2014-07-04 12:25:17 +0200

    one more fix as in 2099634cd4714270743c7d95c114442056a1fe8c

--- WbSkim/macros/plot_all4.sh
+++ WbSkim/macros/plot_all4.sh
@@ -64,12 +64,12 @@ while [ $i -le 2 ]; do
   root -l -q -b DataMCComp4.C+\($d,\"w_Ht_bb\",1,$i\)
 
   root -l -q -b DataMCComp4.C+\($d,\"w_delta_wenu_b\",1,$i\)
-  root -l -q -b DataMCComp4.C+\($d,\"w_delta_wmnu_bb\",1,$i\)
-  root -l -q -b DataMCComp4.C+\($d,\"w_delta_wenu_b\",1,$i\)
+  root -l -q -b DataMCComp4.C+\($d,\"w_delta_wenu_bb\",1,$i\)
+  root -l -q -b DataMCComp4.C+\($d,\"w_delta_wmnu_b\",1,$i\)
   root -l -q -b DataMCComp4.C+\($d,\"w_delta_wmnu_bb\",1,$i\)
   root -l -q -b DataMCComp4.C+\($d,\"w_deltaR_wenu_b\",1,$i\)
-  root -l -q -b DataMCComp4.C+\($d,\"w_deltaR_wmnu_bb\",1,$i\)
-  root -l -q -b DataMCComp4.C+\($d,\"w_deltaR_wenu_b\",1,$i\)
+  root -l -q -b DataMCComp4.C+\($d,\"w_deltaR_wenu_bb\",1,$i\)
+  root -l -q -b DataMCComp4.C+\($d,\"w_deltaR_wmnu_b\",1,$i\)
   root -l -q -b DataMCComp4.C+\($d,\"w_deltaR_wmnu_bb\",1,$i\)
 
   i=$((i+1))
