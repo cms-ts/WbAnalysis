e5ecdcfcf4bc2d5ef5532bcd4665e355b74428cb
Giuseppe Della Ricca <giuseppe.della-ricca@cern.ch>
2014-08-11 22:07:08 +0200

    add lumi info for v10

---
+++ WbSkim/macros/LumiInfo_v10.h
@@ -0,0 +1,136 @@
+string version = "v10";
+
+//////////////////// Integrated luminosity in 1/picobarn
+
+double Lumi2012_ele  = 19767.0; // full 2012 electrons luminosity
+double Lumi2012_muon = 19783.0; // full 2012 muons luminosity
+
+// single period lumi, same for electrons and muons dataset in 1/picobarn
+
+double Lumi_2012A_22Jan =  889.362;
+double Lumi_2012B_22Jan = 4422.000;
+double Lumi_2012C_22Jan = 7137.000;
+double Lumi_2012D_22Jan = 7318.000;
+
+//////////////////////// DY MadGraph
+
+double Ngen_dy = 30459503;
+double Xsec_dy = 3*1177.3; // NNLO
+double eXsec_dy = 0;
+
+//////////////////////// TTbar
+
+double Ngen_tt = 6923750;
+double Xsec_tt = 245.8; // NNLO
+double eXsec_tt = 0.026*Xsec_tt;
+
+//////////////////////// TTbar_FullLept
+
+double Ngen_tt_fl = 12011428;
+double Xsec_tt_fl = 25.8; // NNLO : 245.8*0.324*0.324
+double eXsec_tt_fl = 0.026*Xsec_tt;
+
+//////////////////////// TTbar_SemiLept
+
+double Ngen_tt_sl = 24963676;
+double Xsec_tt_sl = 107.67; // NNLO : 245.8*0.324*0.676*2
+double eXsec_tt_sl = 0.026*Xsec_tt;
+
+//////////////////////// TBar_s
+
+double Ngen_tbar_s = 139974;
+double Xsec_tbar_s = 1.76; // NNLO
+double eXsec_tbar_s = 0;
+
+//////////////////////// TBar_t
+
+double Ngen_tbar_t = 1935072;
+double Xsec_tbar_t = 30.7; // NNLO
+double eXsec_tbar_t = 0;
+
+//////////////////////// TBar_tW
+
+double Ngen_tbar_tw = 493460;
+double Xsec_tbar_tw = 11.1; // NNLO
+double eXsec_tbar_tw = 0;
+
+//////////////////////// T_s
+
+double Ngen_t_s = 259961;
+double Xsec_t_s = 3.97; // NNLO
+double eXsec_t_s = 0;
+
+//////////////////////// T_t
+
+double Ngen_t_t = 3758227;
+double Xsec_t_t = 56.4; // NNLO
+double eXsec_t_t = 0;
+
+//////////////////////// T_tW
+
+double Ngen_t_tw = 497658;
+double Xsec_t_tw = 11.1; // NNLO
+double eXsec_t_tw = 0;
+
+//////////////////////// ZZ
+
+double Ngen_zz = 9799908;
+double Xsec_zz = 8.059; // NLO with CTEQ
+double eXsec_zz = 0.15*Xsec_zz; 
+
+//////////////////////// WZ
+
+double Ngen_wz = 10000283;
+double Xsec_wz = 33.21;    // NLO with CTEQ
+double eXsec_wz = 0.15*Xsec_wz; 
+
+//////////////////////// QCD
+
+double Ngen_qcd = 7529312;
+double Xsec_qcd = 3.64E8; // search the NLO !
+double eXsec_qcd = 0.15*Xsec_qcd; 
+
+//////////////////////// WW
+
+double Ngen_ww = 10000431;
+double Xsec_ww = 54.838; // NLO with CTEQ
+double eXsec_ww = 0.15*Xsec_ww; 
+
+//////////////////////// Wj
+
+double Ngen_wj = 57709905;
+double Xsec_wj = 3*12234.4; // NLO with CTEQ
+double eXsec_wj = 0.15*Xsec_wj; 
+
+//////////////////////// W1j
+
+double Ngen_w1j = 23141598;
+double Xsec_w1j = 5400; // LO
+double eXsec_w1j = 0; 
+
+//////////////////////// W2j
+
+double Ngen_w2j = 34044921;
+double Xsec_w2j = 1750; // LO
+double eXsec_w2j = 0; 
+
+//////////////////////// W3j
+
+double Ngen_w3j = 15539503;
+double Xsec_w3j = 519; // LO
+double eXsec_w3j = 0; 
+
+//////////////////////// W4j
+
+double Ngen_w4j = 13382803;
+double Xsec_w4j = 214; // LO
+double eXsec_w4j = 0; 
+
+//////////////////////// Wbb
+
+double Ngen_wbb = 20646001;
+double Xsec_wbb = 3*46.3; // NLO
+double eXsec_wbb = 0;
+
+///////////////////////
+
